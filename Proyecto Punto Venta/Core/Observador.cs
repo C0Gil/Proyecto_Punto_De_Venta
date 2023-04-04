3e6868dd3bbe6d8dba04a81f86047a35a1f0f423
Gil C. Olivares <gilbertocruz2007@micorreo.upp.edu.mx>
2023-04-04 16:59:32 -0600

    Configuracion Inicial de Interfaces
    Creacion de carpeta que contendra clases que siguen el patron de diseño observer

---
+++ Proyecto Punto Venta/Core/Observador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Proyecto_Punto_Venta.Core
+{
+    internal class Observador : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged; //En caso de error, colocar ? despues de PropertyChangedEventHandler
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
