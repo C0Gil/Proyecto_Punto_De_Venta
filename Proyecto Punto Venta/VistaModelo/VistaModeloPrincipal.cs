182bd1db2ee5c6283eb317afe8c27f207c607dc4
Gil C. Olivares <gilbertocruz2007@micorreo.upp.edu.mx>
2023-04-05 17:50:02 -0600

    Creacion De Vista Ventas
    Creacion de vista ventas, previo a las modificaciones para invocar vistas

---
+++ Proyecto Punto Venta/VistaModelo/VistaModeloPrincipal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto_Punto_Venta.Core;
+namespace Proyecto_Punto_Venta.VistaModelo
+{
+    internal class VistaModeloPrincipal :Observador 
+    {
+    }
+}
