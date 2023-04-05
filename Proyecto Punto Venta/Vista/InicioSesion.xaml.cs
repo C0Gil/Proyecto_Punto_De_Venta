410d44608ab345379e3da78b46881458ac1666c1
ErickBautista1706 <99384297+ErickBautista1706@users.noreply.github.com>
2023-04-04 21:20:28 -0600

    Correcto consumo de web service
    Subir web service login.asmx al IIS para correcto consumo, ademas cambiar linea de direccionServicio si considera necesario

--- Proyecto Punto Venta/Vista/InicioSesion.xaml.cs
+++ Proyecto Punto Venta/Vista/InicioSesion.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ServiceReferenceLogin;
+using System;
 using System.Collections.Generic;
+using System.DirectoryServices;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,6 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
 
 namespace Proyecto_Punto_Venta.Vista
 {
@@ -23,5 +27,36 @@ namespace Proyecto_Punto_Venta.Vista
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Obtener usuario y contraseña de los campos de texto
+            string usuario = txtUser.Text;
+            string contrasena = txtPsw.Text;
+
+            // Crear cliente del servicio web
+            string direccionServicioWeb = "http://localhost/tienda_login_v1/ws/login.asmx";
+            ServiceReferenceLogin.login1SoapClient client = new ServiceReferenceLogin.login1SoapClient(new System.ServiceModel.BasicHttpBinding(), new System.ServiceModel.EndpointAddress(direccionServicioWeb));
+
+            // Llamar a la función de inicio de sesión del servicio web
+            ServiceReferenceLogin.ValidacionSesion resultado = client.ValidarSesion(usuario, contrasena);
+
+            // Verificar el resultado de la validación de sesión
+            if (resultado.Resultado)
+            {
+                // Iniciar sesión exitosa
+                MessageBox.Show("Inicio de sesión exitoso");
+                // Aquí puedes abrir la ventana principal de tu aplicación
+            }
+            else
+            {
+                // Iniciar sesión fallida
+                MessageBox.Show("Inicio de sesión fallido");
+            }
+
+
+
+        }
     }
 }
+    
