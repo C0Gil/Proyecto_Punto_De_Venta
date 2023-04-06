cfe9002d6e28915c197959ecbb5deb9a0f027e32
Gil C. Olivares <gilbertocruz2007@micorreo.upp.edu.mx>
2023-04-05 20:54:57 -0600

    Navegacion Basica
    Navegacion entre ventanas basicas completada

--- Proyecto Punto Venta/VistaModelo/VistaModeloPrincipal.cs
+++ Proyecto Punto Venta/VistaModelo/VistaModeloPrincipal.cs
@@ -8,5 +8,43 @@ namespace Proyecto_Punto_Venta.VistaModelo
 {
     internal class VistaModeloPrincipal :Observador 
     {
+
+        public RelayCommand VentasVMCommand { get; set; }
+
+        public RelayCommand InventarioVMCommand { get; set; }
+
+        public VistaModeloVentas VentasVM { get; set; }
+
+        public VistaModeloInventario InventarioVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public VistaModeloPrincipal()
+        {
+            VentasVM= new VistaModeloVentas();
+            InventarioVM= new VistaModeloInventario();
+
+            CurrentView = VentasVM;
+
+            VentasVMCommand = new RelayCommand(o => 
+            {
+                CurrentView = VentasVM;
+            });
+            InventarioVMCommand = new RelayCommand(o =>
+            {
+                CurrentView = InventarioVM;
+            });
+        }
     }
 }
